4944af63ea641b6850dddc8979b3009be7068ada
Rafael <rafaelmapache@gmail.com>
2019-04-16 20:36:12 +0200

    Quemando gurobito

--- TFM - Rafa/gurobi2.py
+++ TFM - Rafa/gurobi2.py
@@ -1,58 +1,87 @@
 # Comenzamos el codigo para resolver el problema del matching mediante 
 # resolucion directa del problema de Programacion Lineal.
-# Utilizamos Python 3.6 como interfaz para el software Gurobi.
+# Utilizamos Python 3.7 como interfaz para el software Gurobi 8.1.1.
 
 # El siguiente codigo genera una solucion y valor objetivo asociado
 # para cada elemento de un mallado de valores de lambda, de manera que
 # podemos observar como varian ambos en funcion de dicho parametro.
+
+# A continuacion pasamos a programar el algoritmo de Groetschel-Holland 
+# para resolver el problema biobjetivo del emparejamiento con funciones
+# de coste c1, c2 y parametro lambda.
 
 from gurobipy import *
 import numpy as np
 import itertools 
 import operator
 import networkx as nx
+import random 
+
+
+# DATOS DE ENTRADA
+##############################################################################
 
 # Establecemos una semilla predeterminada para asegurar la reproducibilidad
 # de los resultados
 np.random.seed(1234)
+random.seed(1234)
 
 # Construimos un grafo aleatorio
 mat = nx.gnp_random_graph(12,0.6)
 
 # Numerando los vértices a partir de 0, obtenemos la matriz de adyacencia
 # del grafo como un array 
-A = nx.convert_matrix.to_numpy_matrix(mat)
+#A = nx.convert_matrix.to_numpy_matrix(mat)
+A = np.array([[0., 0., 1., 1., 1., 1., 1., 0., 0., 0., 0., 0.],
+        [0., 0., 1., 0., 1., 1., 1., 0., 1., 0., 1., 0.],
+        [1., 1., 0., 1., 1., 0., 1., 0., 1., 1., 0., 0.],
+        [1., 0., 1., 0., 1., 0., 1., 1., 1., 1., 0., 0.],
+        [1., 1., 1., 1., 0., 1., 0., 0., 1., 1., 1., 1.],
+        [1., 1., 0., 0., 1., 0., 1., 0., 1., 1., 1., 1.],
+        [1., 1., 1., 1., 0., 1., 0., 1., 0., 1., 1., 0.],
+        [0., 0., 0., 1., 0., 0., 1., 0., 0., 0., 1., 0.],
+        [0., 1., 1., 1., 1., 1., 0., 0., 0., 1., 1., 0.],
+        [0., 0., 1., 1., 1., 1., 1., 0., 1., 0., 1., 1.],
+        [0., 1., 0., 0., 1., 1., 1., 1., 1., 1., 0., 1.],
+        [0., 0., 0., 0., 1., 1., 0., 0., 0., 1., 1., 0.]])
 
 # Numero de vertices
 N = len(A)
 
-# Construyamos algunas funciones que nos resultaran utiles
-# para describir generalmente el problema
+# Sea M el numero de puntos en el que queremos dividir el intervalo [0,1],
+# es decir, los M+1 un puntos que forman la malla de valores de lambda para
+# los cuales vamos a resolver el problema.
+M = 5
+lambdas = np.array(range(0,M+1,1))/M
+
+# FUNCIONES 
+##############################################################################
+
+# Construimos algunas funciones que nos resultaran utiles.
 
 # Comencemos creando el conjunto de indices que vamos a utilizar
 # para las variables
-def indices(n,a):
+def indices(a):
     s = []
+    n = len(a[0])
     for i in range(n):
         for j in range(i+1,n):
             if (a[i,j] == 1):
                 s.append((i,j))
     return(s)
 
-# Calculamos una vez el conjunto de indices
-IND = indices(N,A)
+# Calculamos una vez el conjunto de indices, que no es mas que las aristas
+# del grafo ordendas de forma lexicografica y sin repeticiciones.
+IND = indices(A)
 
-# Construimos aleatoriamente las funciones de coste
+# Construimos las funciones de coste. En este caso las generamos de forma
+# aleatoria.
 c1 = np.random.rand(len(IND))
 c2 = np.random.rand(len(IND))
     
-# Definimos la funcion delta, que nos da los vertices
-# ejes adyacentes a un vertice dado.
+# Definimos la funcion delta, que nos da los ejes adyacentes a un vertice dado.
 def delta(v):
-    s = []
-    for (a,b) in IND:
-        if (a==v or b==v):
-            s.append((a,b))
+    s = [x for x in IND if x[0] == v or x[1] == v]
     return(s)
 
 # Calculamos una vez el conjunto delta
@@ -71,7 +100,14 @@ def gamma(S):
         if (a in S and b in S):
             m.append((a,b))
     return(m)
-
+    
+# Definimos la operacion diferencia de conjuntos
+def diff(conj1, conj2):
+        conj2 = set(conj2)
+        return [item for item in conj1 if item not in conj2]
+    
+# Definimos una funcion combinatoria para obtener los conjuntos impares
+# de vertices.
 def subconj(N,m):
     S = list(range(N))
     return list(itertools.combinations(S, m))
@@ -81,27 +117,47 @@ IMPARES = []
 for i in range(3,N,2):
     IMPARES = IMPARES + subconj(N,i)
 
-# Sea M el numero de puntos en el que queremos dividir el intervalo [0,1]
-M = 1
-lambdas = np.array(range(0,M+1,1))/M
+# Construimos una funcion que comprueba si un vector es de coordenadas enteras
+def entero(s):
+    a = map(lambda x: x == int(x),s)
+    return(all(a))
 
-def lambdafun(lam):
-    # Creamos un modelo
-    m = model("escenarios");
+# Pasamos a definir la funcion optimiza, que resuelve el problema del
+# emparejamiento parametro con parametros
+# lamb: Valor de lambda
+# c1,c2: Funciones de coste
+# indices: Conjunto de aristas que definen el problema
+
+def optimiza(lamb, c1 = c1, c2 = c2, indices = IND):
     
+    # Paso 1: En primer lugar deberíamos seleccionar un conjunto de aristas
+    # relativamente pequeño para iniciar el algoritmo, pero dado que ya de por
+    # si cuesta lidiar con grafos relativamente grandes, podemos permitirnos
+    # utilizar la cantidad total de aristas, ya que estas crecen a lo sumo de 
+    # forma cuadratica con respecto los vertices. El problema principal es el
+    # de las restricciones, que sabemos con certeza que crecen de manera
+    # exponencial respecto a los vertices.
+    
+    # Paso 2: En este caso, Holland y Groetschel nos indican que utilicemos un 
+    # algoritmo voraz para encontrar una solucion inicial, pero para eso
+    # Gurobi tiene implementado un presolve, así que pasamos a crear el modelo
+    
+    m = Model("biobjetivo");
+
     # Creamos las variables. El tipo puede ser 
     # 'C' para continuas, 'B' para binarias, 'I' para enteras,
     # 'S' para semicontinuas, or 'N' for semienteras.
     # El parametro lb nos da una cota inferior a las variables.
     x = m.addVars(IND, lb = 0, vtype='C', name = "x");
     
-    # Damos la siguiente función para ayudarnos a escribir las restricciones
-    def suma(S):
+    # Definimos algunas funciones para ayudarnos a escribir las restricciones
+    # asi como la funcion objetivo de manera comoda y general.
+    def suma(S,x):
         s = 0
         for (a,b) in S:
             s = s + x[a,b]
         return(s)
-    # También diseñamos una para escribir las funciones objetivo
+        
     def objind(c):
         s = 0
         for i in range(len(IND)):
@@ -113,7 +169,6 @@ def lambdafun(lam):
     # escribimos directamente los costes sobre las variables aristas
     # del modelo, pues si una arista no está en el grafo, no tiene sentido
     # generar una variable para ella.       
-
     cos1 = objind(c1)
     cos2 = objind(c2)
     
@@ -121,45 +176,31 @@ def lambdafun(lam):
     m.ModelSense = GRB.MINIMIZE
     
     # Añadimos la funcion objetivo
-    m.setObjective((1-lam)*cos1+lam*cos2); 
+    m.setObjective((1-lamb)*cos1+lamb*cos2); 
     
-    # Pasamos a generar las restricciones.
-    # Imponemos que cada vertice tenga una pareja.
+    # Generamos el primer bloque de restricciones.
     for i in range(N):
         m.addConstr(suma(delta(i)) == 1);
     
-    m.setParam('Basis',2)
+    m.optimice()
+    
     # Imponemos las condiciones sobre los conjuntos impares.
     for a in IMPARES:
         S = gamma(a)
         card = len(a)-1
         m.addConstr(suma(S) <= card/2)
-        
-    print(m.getParamInfo('basis'))
-    m.optimize();
+    print(m.getVars())
+    # En el caso que decidamos utilizar una base inicial ejecutamos
+    if (vbas != []):
+        m.update()
+        for i in range(len(vbas)):
+            m.getVars()[i].setAttr("VBasis",vbas[i])
+        for i in range(len(cbas)):
+            m.getConstrs()[i].setAttr("CBasis",cbas[i])
     
-    # Preparamos la salida por pantalla
-    s=[]
-    for i in range(len(x)):
-        q = m.getVars()
-        v = q[i]
-        s.append(v.x)
-    return([m.objVal,s])
-
-
-m = []
-for i in lambdas:
-    m.append(lambdafun(i))    
-
-sols = [a[1] for a in m]
-
-#for i in range(len(sols)):
-#    print(*sols[i])
+    # Resolvemos el modelo
+    m.optimize()
     
-def unique(list1):
-    unique_list = [] 
-    for x in list1:
-        if x not in unique_list:
-            unique_list.append(x)
-    return(unique_list)
-    
+    return(m)
+
+#    
